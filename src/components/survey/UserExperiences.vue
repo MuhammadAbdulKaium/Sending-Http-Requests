1d7c9c02c93c9f687a3426768a56644b55ffb326
MuhammadAbdulKaium <makaium33@gmail.com>
2020-12-09 15:40:34 +0600

    156. Showing a "Loading..." Message, while loading.

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -5,7 +5,8 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -28,15 +29,18 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false,
     }
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch('https://vue-http-demo-bcfbe-default-rtdb.firebaseio.com/surveys.json').then((response) => {
         if(response.ok) {
           return response.json();
         }
       }).then((data) => {
+        this.isLoading = false;
         const results = [];
         for (const id in data) {
           results.push({
